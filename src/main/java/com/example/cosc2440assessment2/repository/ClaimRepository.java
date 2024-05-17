b337e64819ea17611351cc27d3274a7fda6c7039
martin <martin.delahousse@epitech.eu>
2024-05-17 10:00:57 +0700

    [ADD] claim repository

---
+++ src/main/java/com/example/cosc2440assessment2/repository/ClaimRepository.java
@@ -0,0 +1,34 @@
+package com.example.cosc2440assessment2.repository;
+
+import com.example.cosc2440assessment2.model.Claim;
+import com.example.cosc2440assessment2.singleton.Database;
+import java.util.List;
+
+public class ClaimRepository {
+    private static ClaimRepository INSTANCE;
+    private final Database database = Database.getInstance();
+
+    public void addClaim(Claim claim) {
+    }
+
+    public void updateClaim(Claim claim) {
+    }
+
+    public void deleteClaim(Claim claim) {
+
+    }
+
+    public Claim getClaimByUsername(String username) {
+        return null;
+    }
+
+    public List<Claim> getAllClaims() {
+        return null;
+    }
+
+    public static ClaimRepository getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new ClaimRepository();
+        return INSTANCE;
+    }
+}
