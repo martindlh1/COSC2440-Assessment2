8fb5ca1fe0a587041fd7920d58379f769e55a8b7
Touxooo <thomas.rabiet@epitech.eu>
2024-05-20 16:19:29 +0700

    [ADD] get all claims repo + service + sql

--- src/main/java/com/example/cosc2440assessment2/sql/ClaimSQLCommand.java
+++ src/main/java/com/example/cosc2440assessment2/sql/ClaimSQLCommand.java
@@ -8,6 +8,10 @@ public class ClaimSQLCommand {
         return "select user_.\"uID\", claim.* from user_, claim where username = '" + username + "' and \"uID\" = claim.\"cID\";";
     }
 
+    public static String getAllClaims() {
+        return "select claim.*, user_.\"uID\", user_.fullname from claim, user_ where user_.\"uID\" = claim.\"cID\";";
+    }
+
     public static String addClaim(Claim claim) {
         String sql = "insert into claim (\"cID\", \"cardID\", \"date\", \"exam_date\", \"amount\", \"status\") values (" + claim.getInsured() + ", " + claim.getCard().getId() + ", '" + claim.getDate() + "', '" + claim.getExam_date() + "', " + claim.getAmount() + ", '" + claim.getState() + "');";
         System.out.println(sql);
