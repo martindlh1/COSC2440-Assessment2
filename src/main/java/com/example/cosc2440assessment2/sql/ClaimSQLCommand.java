aece3d5015d9f9257929158aeb6732996a09af2d
Touxooo <thomas.rabiet@epitech.eu>
2024-05-20 10:25:39 +0700

    [ADD & MOD] switched to user_ table + policy owner get claims

--- src/main/java/com/example/cosc2440assessment2/sql/ClaimSQLCommand.java
+++ src/main/java/com/example/cosc2440assessment2/sql/ClaimSQLCommand.java
@@ -1,7 +1,13 @@
 package com.example.cosc2440assessment2.sql;
 
+import com.example.cosc2440assessment2.model.Claim;
+
 public class ClaimSQLCommand {
     public static String getClaimsByUsername(String username) {
         return "select user_.\"uID\", claim.* from user_, claim where username = '" + username + "' and \"uID\" = claim.\"cID\";";
+    }
+
+    public static String addClaim(Claim claim) {
+
     }
 }
